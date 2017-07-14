085bdfdba077e91319e7a2bb4382d80519375880
huangliang6 <huangliang6@lenovo.com>
2017-07-14 11:08:07 +0800

    fix docker registery port

--- genImages.sh
+++ genImages.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-docker build -t 127.0.0.1:5002/dmall/config-server:$BUILD_NUMBER .
-docker tag 127.0.0.1:5002/dmall/config-server:$BUILD_NUMBER 127.0.0.1:5002/dmall/config-server:latest
+docker build -t 127.0.0.1:5000/dmall/config-server:$BUILD_NUMBER .
+docker tag 127.0.0.1:5000/dmall/config-server:$BUILD_NUMBER 127.0.0.1:5002/dmall/config-server:latest
 
-docker push 127.0.0.1:5002/dmall/config-server:$BUILD_NUMBER
-docker push 127.0.0.1:5002/dmall/config-server:latest
+docker push 127.0.0.1:5000/dmall/config-server:$BUILD_NUMBER
+docker push 127.0.0.1:5000/dmall/config-server:latest
 
