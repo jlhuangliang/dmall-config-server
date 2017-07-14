57af4d68fc028c87802ccb73faeb2f56f055b49b
huangliang6 <huangliang6@lenovo.com>
2017-07-14 23:39:48 +0800

    fix jenkinsfile.

--- genImages.sh
+++ genImages.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker build -t dmall-docker-registry:5000/dmall/config-server:$BUILD_NUMBER .
-docker tag dmall-docker-registry:5000/dmall/config-server:$BUILD_NUMBER dmall-docker-registry:5002/dmall/config-server:latest
+docker tag dmall-docker-registry:5000/dmall/config-server:$BUILD_NUMBER dmall-docker-registry:5000/dmall/config-server:latest
 
 docker push dmall-docker-registry:5000/dmall/config-server:$BUILD_NUMBER
 docker push dmall-docker-registry:5000/dmall/config-server:latest
